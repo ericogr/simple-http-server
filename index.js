cfbd113c8b17ed92b728c984b7bd3df2181a6fee
Erico <ericogr@gmail.com>
2018-02-15 12:01:34 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.argv[2] ? process.argv[2] : 8080
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+function genericResponse(request, response) {
+  console.log(`Date time: ${new Date().toString()}`)
+  console.log(`Method: ${request.method}`)
+  console.log(`URL: ${request.url}`)
+  console.log(`Receive Body: ${JSON.stringify(request.body, null, 2)}`)
+
+  let retorno = {
+        "id": Math.floor(Math.random() * 1000000)
+  }
+
+  console.log(`Send body: ${JSON.stringify(retorno)}\n`)
+
+  response.send(retorno)
+}
+
+app.get('/*', genericResponse);
+app.put('/*', genericResponse);
+app.post('/*', genericResponse);
+app.delete('/*', genericResponse);
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}\n`)
+})
