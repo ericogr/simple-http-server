466cb9a32cb9fbb53faa7c0191ec44b483172ff9
Érico <ericogr@gmail.com>
2018-09-29 13:30:11 -0300

    support uploading

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const busboy = require('connect-busboy')
 
 const app = express()
 const port = process.argv[2] ? process.argv[2] : 8080
@@ -7,26 +8,46 @@ const timeout = process.argv[3] ? process.argv[3] : 0
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(busboy())
 
 function genericResponse(request, response) {
-  console.log(`Date time: ${new Date().toString()}`)
-  console.log(`Method: ${request.method}`)
-  console.log(`URL: ${request.url}`)
-  console.log('Headers:')
-  Object.keys(request.headers).forEach((hName) => {
-    console.log(` - ${hName}: ${request.headers[hName]}`)
-  })
-  console.log(`Receive Body: ${JSON.stringify(request.body, null, 2)}`)
+    console.log(`Date time: ${new Date().toString()}`)
+    console.log(`Method: ${request.method}`)
+    console.log(`URL: ${request.url}`)
+    console.log('Headers:')
+    Object.keys(request.headers).forEach((hName) => {
+        console.log(` - ${hName}: ${request.headers[hName]}`)
+    })
 
-  let retorno = {
+    let retorno = {
         "id": Math.floor(Math.random() * 1000000)
-  }
+    }
 
-  console.log(`Send body: ${JSON.stringify(retorno)}\n`)
+    if (request.busboy) {
+        request.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            console.log(`Receiving uploaded data from file ${filename}`)
 
-  setTimeout(() => {
-    response.send(retorno)
-  }, timeout)
+            file.on('data', function(data) {
+                console.log(data.toString());
+            })
+
+            file.on('end', function() {
+                console.log('upload finished')
+                console.log(`Send body back: ${JSON.stringify(retorno)}\n`)
+                response.send(retorno)
+                console.log('================================================================================')
+            })
+        })
+        request.pipe(request.busboy);
+    }
+    else {
+        console.log(`Receive Body: ${JSON.stringify(request.body, null, 2)}`)
+        setTimeout(() => {
+            console.log(`Send body: ${JSON.stringify(retorno)}\n`)
+            response.send(retorno)
+            console.log('================================================================================')
+        }, timeout)
+    }
 }
 
 app.get('/*', genericResponse);
@@ -35,12 +56,12 @@ app.post('/*', genericResponse);
 app.delete('/*', genericResponse);
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-  console.log(
+    console.log(
     `server up
-      - listening on ${port}
-      - timeout ${timeout}\n`)
+    - listening on ${port}
+    - timeout ${timeout}\n`)
 })
